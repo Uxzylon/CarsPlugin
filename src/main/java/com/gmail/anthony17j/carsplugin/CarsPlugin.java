e0b63cab8f401c87dabdaf47efd68418ad66020a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-15 18:02:45 +0200

    Adjusted default config

--- src/main/java/com/gmail/anthony17j/carsplugin/CarsPlugin.java
+++ src/main/java/com/gmail/anthony17j/carsplugin/CarsPlugin.java
@@ -5,9 +5,6 @@ import com.gmail.anthony17j.carsplugin.Events.carClickEvent;
 import com.gmail.anthony17j.carsplugin.Events.playerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public final class CarsPlugin extends JavaPlugin {
 
     public static CarsPlugin plugin;
@@ -20,15 +17,15 @@ public final class CarsPlugin extends JavaPlugin {
         this.getConfig().options().copyDefaults(true);
         this.getConfig().options().header("CarsPlugin config file");
         this.getConfig().addDefault("CarsList.Test.CustomModelData", 4);
-        this.getConfig().addDefault("CarsList.Test.Seats.1.X", 0);
-        this.getConfig().addDefault("CarsList.Test.Seats.1.Y", 1);
+        this.getConfig().addDefault("CarsList.Test.Seats.1.X", -1);
+        this.getConfig().addDefault("CarsList.Test.Seats.1.Y", -0.5);
         this.getConfig().addDefault("CarsList.Test.Seats.1.Z", 0);
-        this.getConfig().addDefault("CarsList.Test.Seats.2.X", 0);
-        this.getConfig().addDefault("CarsList.Test.Seats.2.Y", 0);
+        this.getConfig().addDefault("CarsList.Test.Seats.2.X", -1);
+        this.getConfig().addDefault("CarsList.Test.Seats.2.Y", -0.3);
         this.getConfig().addDefault("CarsList.Test.Seats.2.Z", 1);
-        this.getConfig().addDefault("CarsList.Test.Seats.3.X", 1);
-        this.getConfig().addDefault("CarsList.Test.Seats.3.Y", 0);
-        this.getConfig().addDefault("CarsList.Test.Seats.3.Z", 0);
+        this.getConfig().addDefault("CarsList.Test.Seats.3.X", -1);
+        this.getConfig().addDefault("CarsList.Test.Seats.3.Y", -0.3);
+        this.getConfig().addDefault("CarsList.Test.Seats.3.Z", -1);
         this.getConfig().addDefault("CarsList.Test.breakingSpeed", 0.03);
         this.getConfig().addDefault("CarsList.Test.maxSpeed", 0.7);
         this.getConfig().addDefault("CarsList.Test.maxSpeedBackwards", 0.35);
